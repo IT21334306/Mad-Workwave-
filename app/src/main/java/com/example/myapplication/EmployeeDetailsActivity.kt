81f6249ec192221f4aad3b5293500e6fd4c21888
Dasunika Ekanayake <it21174162@my.sliit.lk>
2023-05-05 18:16:57 +0530

    1st one

---
+++ app/src/main/java/com/example/myapplication/EmployeeDetailsActivity.kt
@@ -0,0 +1,70 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
+class EmployeeDetailsActivity : AppCompatActivity() {
+
+    private lateinit var tvEmpId: TextView
+    private lateinit var tvEmpName: TextView
+    private lateinit var tvEmpAge: TextView
+    private lateinit var tvEmpSalary: TextView
+    private lateinit var btnUpdate: Button
+    private lateinit var btnDelete: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_details)
+
+        initView()
+        setValuesToViews(intent)
+
+        btnUpdate.setOnClickListener {
+            openUpdateDialog()
+        }
+
+        btnDelete.setOnClickListener {
+            deleteRecord(intent.getStringExtra("empId").toString())
+        }
+    }
+
+    private fun initView() {
+        tvEmpId = findViewById(R.id.tvEmpId)
+        tvEmpName = findViewById(R.id.tvEmpName)
+        tvEmpAge = findViewById(R.id.tvEmpAge)
+        tvEmpSalary = findViewById(R.id.tvEmpSalary)
+
+        btnUpdate = findViewById(R.id.btnUpdate)
+        btnDelete = findViewById(R.id.btnDelete)
+    }
+
+    private fun setValuesToViews(intent: Intent) {
+        tvEmpId.text = intent.getStringExtra("empId")
+        tvEmpName.text = intent.getStringExtra("empName")
+        tvEmpAge.text = intent.getStringExtra("empAge")
+        tvEmpSalary.text = intent.getStringExtra("empSalary")
+    }
+
+    private fun deleteRecord(id: String) {
+        val dbRf = FirebaseDatabase.getInstance().getReference("Employees").child(id)
+        val mTask =  dbRf.removeValue()
+
+        mTask.addOnSuccessListener {
+            Toast.makeText(this, "Employee data deleted", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, FetchingActivity::class.java)
+            finish()
+            startActivity(intent)
+        }.addOnFailureListener{ error ->
+            Toast.makeText(this, "Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun openUpdateDialog() {
+        // TODO: Implement the update dialog
+    }
+}
