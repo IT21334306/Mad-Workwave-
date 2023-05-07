5175ab88836a79272e021760058fb19e123a5779
Dasunika Ekanayake <it21174162@my.sliit.lk>
2023-05-07 14:09:58 +0530

    initial commit

--- app/src/main/java/com/example/myapplication/EmployeeDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/EmployeeDetailsActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
@@ -17,20 +19,21 @@ class EmployeeDetailsActivity : AppCompatActivity() {
     private lateinit var btnUpdate: Button
     private lateinit var btnDelete: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_details)
 
         initView()
-        setValuesToViews(intent)
+        setValuesToViews()
 
-        btnUpdate.setOnClickListener {
-            openUpdateDialog()
+        btnUpdate.setOnClickListener{
+            OpenUpdateDialog(
+                intent.getStringExtra("empId").toString(),
+                intent.getStringExtra("empName").toString()
+            )
         }
 
-        btnDelete.setOnClickListener {
-            deleteRecord(intent.getStringExtra("empId").toString())
-        }
     }
 
     private fun initView() {
@@ -43,28 +46,66 @@ class EmployeeDetailsActivity : AppCompatActivity() {
         btnDelete = findViewById(R.id.btnDelete)
     }
 
-    private fun setValuesToViews(intent: Intent) {
+    private fun setValuesToViews() {
         tvEmpId.text = intent.getStringExtra("empId")
         tvEmpName.text = intent.getStringExtra("empName")
         tvEmpAge.text = intent.getStringExtra("empAge")
         tvEmpSalary.text = intent.getStringExtra("empSalary")
+
     }
 
-    private fun deleteRecord(id: String) {
-        val dbRf = FirebaseDatabase.getInstance().getReference("Employees").child(id)
-        val mTask =  dbRf.removeValue()
+    private fun OpenUpdateDialog(
+        empId: String,
+        empName: String
+    ) {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.update_dialog, null)
 
-        mTask.addOnSuccessListener {
-            Toast.makeText(this, "Employee data deleted", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, FetchingActivity::class.java)
-            finish()
-            startActivity(intent)
-        }.addOnFailureListener{ error ->
-            Toast.makeText(this, "Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
+        mDialog.setView(mDialogView)
+
+        val etEmpName = mDialogView.findViewById<EditText>(R.id.etEmpName)
+        val etEmpAge = mDialogView.findViewById<EditText>(R.id.etEmpAge)
+        val etEmpSalary = mDialogView.findViewById<EditText>(R.id.etEmpSalary)
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        etEmpName.setText(intent.getStringExtra("empName").toString())
+        etEmpAge.setText(intent.getStringExtra("empAge").toString())
+        etEmpSalary.setText(intent.getStringExtra("empSalary").toString())
+
+        mDialog.setTitle("Updating $empName Record")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener {
+            updateEmpData(
+                empId,
+                etEmpName.text.toString(),
+                etEmpAge.text.toString(),
+                etEmpSalary.text.toString()
+            )
+
+            Toast.makeText(applicationContext, "Employee Data Updated", Toast.LENGTH_LONG).show()
+
+            //we are setting updated data to our textviews
+            tvEmpName.text = etEmpName.text.toString()
+            tvEmpAge.text = etEmpAge.text.toString()
+            tvEmpSalary.text = etEmpSalary.text.toString()
+
+            alertDialog.dismiss()
+
         }
     }
+    private fun updateEmpData(
+        id:String,
+        name: String,
+        age: String,
+        salary : String
+    ){
+        val dbRef = FirebaseDatabase.getInstance().getReference("Employees").child(id)
+        val empInfo = EmployeeModel(id,name,age,salary)
+        dbRef.setValue(empInfo)
 
-    private fun openUpdateDialog() {
-        // TODO: Implement the update dialog
     }
 }
